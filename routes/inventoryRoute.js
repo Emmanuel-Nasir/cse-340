f05b3c05b240e6f90a491af046405052a8d7868e
Emmanuel Nasir <emmanuel.nasi@gmail.com>
2025-09-27 15:59:06 -0700

    trying

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,27 +1,27 @@
-// const express = require("express")
-// const router = express.Router()
-// const invController = require("../controllers/invController")
-// const invValidate = require("../utilities/inventory-validation")
+const express = require("express")
+const router = express.Router()
+const invController = require("../controllers/invController")
+const invValidate = require("../utilities/inventory-validation")
 
-// // Task 1: Management view
-// router.get("/", invController.buildManagement)
+// Task 1: Management view
+router.get("/", invController.buildManagement)
 
-// // Task 2: Add Classification
-// router.get("/add-classification", invController.buildAddClassification)
-// router.post(
-//   "/add-classification",
-//   invValidate.classificationRules(),
-//   invValidate.checkClassificationData,
-//   invController.addClassification
-// )
+// Task 2: Add Classification
+router.get("/add-classification", invController.buildAddClassification)
+router.post(
+  "/add-classification",
+  invValidate.classificationRules(),
+  invValidate.checkClassificationData,
+  invController.addClassification
+)
 
-// // Task 3: Add Inventory
-// router.get("/add-inventory", invController.buildAddInventory)
-// router.post(
-//   "/add-inventory",
-//   invValidate.inventoryRules(),
-//   invValidate.checkInventoryData,
-//   invController.addInventory
-// )
+// Task 3: Add Inventory
+router.get("/add-inventory", invController.buildAddInventory)
+router.post(
+  "/add-inventory",
+  invValidate.inventoryRules(),
+  invValidate.checkInventoryData,
+  invController.addInventory
+)
 
-// module.exports = router
+module.exports = router
